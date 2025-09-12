b545dde1a8fe26469ee68f6763ae1448be7b46ee
Guillermo24-10 <pgaugusto24@gmail.com>
2025-09-11 23:17:18 -0500

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Autor/Modelo/AutorLibro.cs
@@ -0,0 +1,12 @@
+﻿namespace TiendaServicios.Api.Autor.Modelo
+{
+    public class AutorLibro
+    {
+        public int AutorLibroId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public DateTime? FechaNacimiento { get; set; }
+        public ICollection<GradoAcademico> ListaGradoAcademico { get; set; } = new List<GradoAcademico>();
+        public string AutorLibroGuid { get; set; } = string.Empty;
+    }
+}
