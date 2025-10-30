1e254985c19cd4c1bf424918ece25f25ba4f4f28
Guillermo24-10 <pgaugusto24@gmail.com>
2025-10-29 19:36:44 -0500

    se implemento api getway ocelot

---
+++ TiendaServicios.Api.Gateway/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Ocelot.DependencyInjection;
+
+namespace TiendaServicios.Api.Gateway.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOcelotConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOcelot(configuration);
+            return services;
+        }
+    }
+}
